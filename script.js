d683fb68808a06a8c30d3b2befbc132a712132bd
ehimare okojie <ehimareokojie@yahoo.com>
2023-10-03 23:04:23 +0100

    commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue Whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "What is 2+2?",
+        answers: [
+            { text: "6", correct: false},
+            { text: "1", correct: false},
+            { text: "22", correct: false},
+            { text: "4", correct: true},
+        ]
+    },
+    {
+        question: "How many states are in Nigeria?",
+        answers: [
+            { text: "34", correct: false},
+            { text: "35", correct: false},
+            { text: "36", correct: true},
+            { text: "33", correct: false},
+        ]
+    },
+    {
+        question: "What is 8+8?",
+        answers: [
+            { text: "16", correct: true},
+            { text: "88", correct: false},
+            { text: "18", correct: false},
+            { text: "17", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtonElement = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    // Clear previous answer buttons
+    answerButtonElement.innerHTML = "";
+
+    const currentQuestion = questions[currentQuestionIndex];
+    const questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        button.addEventListener("click", () => checkAnswer(answer));
+        answerButtonElement.appendChild(button);
+    });
+}
+
+function checkAnswer(answer) {
+    if (answer.correct) {
+        score++;
+    }
+    // Move to the next question
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        // Quiz is over
+        questionElement.innerHTML = `Your Score: ${score} out of ${questions.length}`;
+        answerButtonElement.innerHTML = "";
+        nextButton.innerHTML = "Restart";
+        nextButton.addEventListener("click", startQuiz);
+    }
+}
+
+startQuiz();
